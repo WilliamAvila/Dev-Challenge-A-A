ebcc7fb5a8c9488a682c5b5fafc413391f5f9640
William <william.zx1@gmail.com>
2015-10-04 09:25:59 -0600

    IronMan and TheIncredibleHulk algorithms verified

---
+++ DevChallenge/Avengers.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Microsoft.SqlServer.Server;
+
+namespace DevChallenge
+{
+    public class Avengers
+    {
+        private List<double> fibo;
+        private char[] validVowels = {'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
+
+        private string[] englishWords =
+        {
+            "drool", "cats", "clean", "code", "dogs", "material", "needed", "this", "is", "hard",
+            "what", "are", "you", "smoking", "shot", "gun", "down", "river", "super", "man", "rule", "acklen",
+            "developers", "amazing", "home", "notch", "light", "saw"
+        };
+
+        private string[] testArray = {"hEllo", "bOok", "read", "NeEd", "paliNdromE", "happy", "WhAtArEyOuSmOkInG"};
+        string[] testArray2 = {"lightsaw","WhAtArEyOuSmOkInG","haaha"};
+
+        public Avengers()
+        {
+            fibo = FibonacciList(10000);
+        }
+
+
+        public string IronMan(string[] input)
+        {
+            var result = "";
+            // step #1
+            Array.Sort(input);
+            // step #2
+            string[] newWords = GetMovedVowelsArray(input);
+            // step #3
+            result = ConcatenateAscci(newWords);
+            //step #4
+            var resultEncoded = Base64Encode(result);
+
+            return resultEncoded;
+
+        }
+
+        public string TheIncredibleHulk(string[] input)
+        {
+            var result = "";
+            // step #1
+            string[] newWords = GetMovedVowelsArray(input);
+
+            // step #2
+            Array.Sort(newWords);
+            Array.Reverse(newWords);
+
+            // step #3
+            result = string.Join("*", newWords);
+
+            //step #4
+            var resultEncoded = Base64Encode(result);
+
+            return resultEncoded;
+
+        }
+
+        public string Thor(string[] input, int fib)
+        {
+            var result = "";
+	        // step #1
+            string[] words = AlternateConstants(input);
+
+            // step #2
+            Array.Sort(words);
+
+	        // step #3
+            string[] newWords = SeparateEnglishWords(words);
+
+	        // step #4
+	        var fiboWords = ReplacedVowelsWithFibonacci(newWords, fib);
+	        
+	        // step #5
+	        result = string.Join("*", fiboWords);
+
+	        //step #6
+	         var resultEncoded = Base64Encode(result);
+
+	        return resultEncoded;
+        }
+
+        public string CaptainAmerica(string[] input, int fib)
+        {
+            // step #1
+            string[] words = GetMovedVowelsArray(input);
+
+            // step #2
+            Array.Sort(words);
+
+            // step #3
+            var fiboWords = ReplacedVowelsWithFibonacci(words, fib);
+
+            // step #4
+            var result = ConcatenateAscci(fiboWords);
+
+            //step #5
+            var resultEncoded = Base64Encode(result);
+
+            return resultEncoded;
+        }
+
+        public string[] ReplacedVowelsWithFibonacci(string[] words, int fib)
+        {
+
+            var result = string.Join(",", words);
+            var strBldr = new StringBuilder(result);
+            string newWords = "";
+            foreach (var letter in result)
+            {
+                if (validVowels.Contains(letter))
+                {
+                    newWords += fibo[fib];
+                    fib++;
+                }
+                else
+                {
+                    newWords += letter;
+                }
+            }
+
+
+            return newWords.Split(',');
+        }
+
+        public string[] SeparateEnglishWords(string [] input)
+        {
+            var singleWord = true;
+            List<string> newWords = new List<string>();
+
+            if (input.Length > 1)
+            {
+                foreach (var item in input)
+                {
+                    foreach (var word in englishWords)
+                    {
+                       
+                        var index = item.ToLower().IndexOf(word);
+                        if (index != -1)
+                        {
+                            var subS = item.Substring(index, word.Length);
+                            newWords.Add(subS);
+                            singleWord = false;
+                        }
+                    }
+                    if (singleWord)
+                        newWords.Add(item);
+                    singleWord = true;
+                }
+            }
+            return newWords.ToArray();
+        }
+
+        public string [] AlternateConstants(string [] input)
+        {
+            string[] newWords = input;
+            var alternate =true;
+
+            for(var i =0;i<newWords.Length;i++){
+            
+            	for(var j = 0;j< newWords[i].Length;j++)
+            	{
+
+                    if (!validVowels.Contains(newWords[i][j]))
+                    {
+            			if(alternate)
+            			{
+                            var sbl = new StringBuilder(newWords[i]);
+            				string cons = newWords[i][j].ToString().ToUpper();
+            				sbl[j] = Convert.ToChar(cons);
+            				newWords[i] = sbl.ToString();
+            				alternate =false;
+            			}else{
+                            var sbl = new StringBuilder(newWords[i]);
+                            string cons = newWords[i][j].ToString().ToLower();
+                            sbl[j] = Convert.ToChar(cons);
+                            newWords[i] = sbl.ToString();
+            				alternate = true;
+            			}
+            		}
+            	}
+            }
+            return newWords;
+        }
+
+
+        public List<double> FibonacciList(int num)
+        {
+            var fib = new List<double> {0, 1};
+            for (var i = 2; i < num; i++)
+                fib.Add(fib[i - 1] + (fib[i - 2]));
+
+            return fib;
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+
+        public string ConcatenateAscci(string[] words)
+        {
+            var result = "";
+            result = words[0] + (int)words[words.Length - 1][0];
+            for (var i = 1; i < words.Length; i++)
+            {
+                
+                result += words[i] + (int)words[i -1][0];
+            }
+            return result;
+        }
+
+
+        public string[] GetMovedVowelsArray(string[] input)
+        {
+            string[] newWords = input;
+            for (var i = 0; i < input.Length; i++)
+                newWords[i] = MoveOneVowelToTheRight(input[i]);
+
+            return newWords;
+        }
+
+
+        public string MoveOneVowelToTheRight(string word)
+        {
+            var newWord = word;
+            for (var i = 0; i < word.Length; i++)
+            {
+                if (i < word.Length - 1)
+                {
+                    if (validVowels.Contains(word[i]) && !validVowels.Contains(word[i + 1]))
+                    {
+                        newWord = Swap(newWord, i, i + 1);
+                    }
+                    else if (validVowels.Contains(word[i]) && validVowels.Contains(word[i + 1]))
+                    {
+                        newWord = Swap(newWord, i, i + 1);
+                        i++;
+                    }
+
+                }
+                else
+                {
+                    if (validVowels.Contains(word[i]))
+                        newWord = Swap(newWord, i, i + 1);
+
+                    break;
+                }
+            }
+
+            return newWord;
+        }
+
+
+        public string Swap(string value, int origin, int dest)
+        {
+
+            string newStr = "";
+            if (dest >= value.Length)
+            {
+
+                newStr = value[dest - 1] + value.Substring(0, value.Length - 1);
+            }
+            else
+            {
+                newStr = value;
+                var temp = newStr[dest];
+                var temp2 = newStr[origin];
+                var sb = new StringBuilder(value);
+                sb[origin] = temp;
+                sb[dest] = temp2;
+                newStr = sb.ToString();
+            }
+            return newStr;
+        }
+
+
+    }
+}
---
+++ DevChallenge/ChallengeModule.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.ModelBinding;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace DevChallenge
+{
+    public class ChallengeModule : NancyModule
+    {
+
+        string guid;
+        private string apiUrl = "http://internal-devchallenge-2-dev.apphb.com";
+        string webhookUrl = "http://7088ea79.ngrok.io";
+        string repoUrl = "https://github.com/WilliamAvila/Dev-Challenge-A-A";
+        private Avengers avenger;
+        private List<string> results; 
+        public ChallengeModule()
+        {
+            Avengers av;
+            Get["/"] = _ =>
+            {
+                avenger = new Avengers();
+                results = new List<string>();
+                string response = "";
+                for (int i = 0; i < 20; i++)
+                {
+                    var obj = HttpGet();
+                    var respo = JsonConvert.DeserializeObject<ResponseObject>(obj);
+
+                    var encodedValue = ApplyAlgorithm(respo.words, respo.startingFibonacciNumber, respo.algorithm);
+
+                    var responseP = HttpPost(encodedValue, respo.algorithm);
+                    var responsePost = JsonConvert.DeserializeObject<ResponsePost>(responseP);
+                    results.Add(responseP);
+
+                }
+
+                
+                return response;
+            };
+
+            Post["/"] = _ =>
+            {
+                var response = this.Bind<ResponsePayload>();
+                return response.secret;
+            };
+
+        }
+
+       
+
+        public string GetGuid()
+        {
+            Guid g;
+            g = Guid.NewGuid();
+            return g.ToString();
+        }
+
+
+
+        public string HttpGet()
+        {
+            guid = GetGuid();
+            var client = new RestClient(apiUrl);
+            var request = new RestRequest("values/" + guid, Method.GET);
+
+            var response =  client.Execute(request);
+            var content = response.Content;
+            return content;
+        }
+
+
+        public string HttpPost(string encodedValue, string algorithm)
+        {
+            var client = new RestClient(apiUrl);
+            var request = new RestRequest("/values/" + guid + "/" + algorithm, Method.POST);
+
+            request.AddParameter("encodedValue", encodedValue);
+            request.AddParameter("emailAddress", "william@acklenavenue.com");
+            request.AddParameter("name", "William Avila");
+            request.AddParameter("webhookUrl", webhookUrl);
+            request.AddParameter("repoUrl", repoUrl);
+
+            var response = client.Execute(request);
+            var content = response.Content;
+
+            return content;
+            
+        }
+
+        public string ApplyAlgorithm(string[] words, double startingFibonacciNumber, string algorithm)
+        {
+            var result = "";
+            int fib = (int)startingFibonacciNumber;
+            if (algorithm == "IronMan")
+                result = avenger.IronMan(words);
+            else if (algorithm == "TheIncredibleHulk")
+                result = avenger.TheIncredibleHulk(words);
+            else if (algorithm == "Thor")
+                result = avenger.Thor(words, fib);
+            else if (algorithm == "CaptainAmerica")
+                result = avenger.CaptainAmerica(words,fib);
+
+            return result;
+        }
+    }
+
+    public class ResponsePayload
+    {
+        public string secret { get; set; }
+    }
+
+    public class ResponseObject
+    {
+        public string[] words { get; set; }
+        public double startingFibonacciNumber { get; set; }
+        public string algorithm { get; set; }
+    }
+
+    internal class ResponsePost
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+    }
+}
